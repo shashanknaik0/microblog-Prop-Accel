71baea10ba43e9c41d654574fb3fbc74f845ae7b
Shashank Naik <shashankkrishnanaik@gmail.com>
2023-07-04 15:09:12 +0530

    Add. logic to send serialized data

--- users/serializer.py
+++ users/serializer.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import User
  
-class FoodSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model=User
         fields=("id", "username", "email", "password")
--- users/views.py
+++ users/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .serializer import UserSerializer
+from .models import User
 
 # Create your views here.
 @api_view(['GET'])
-def getUser(request):
-    return Response()
+def getUser(request, user_id):
+    user = User.objects.get(id = user_id)
+    serializer = UserSerializer(user)
+    return Response(serializer.data)
