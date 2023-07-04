69aedf6978feeee2ccfd5299fe4a40ccb51d1753
Shashank Naik <shashankkrishnanaik@gmail.com>
2023-07-04 13:59:28 +0530

    Add. postgres setting and app user

--- microblog/settings.py
+++ microblog/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'microblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'microblog',
+       'USER': 'postgres',
+       'PASSWORD': 'shashank',
+       'HOST': 'localhost',
+       'PORT': '5432',
     }
 }
 
