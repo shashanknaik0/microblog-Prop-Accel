eb6139d5a60dcfc790656f657896db836108f447
Shashank Naik <shashankkrishnanaik@gmail.com>
2023-07-04 17:09:57 +0530

    Add. handle exeption logic

--- users/views.py
+++ users/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 from .serializer import UserSerializer
 from .models import User
 
 # Create your views here.
 @api_view(['GET'])
 def getUser(request, user_id):
-    user = User.objects.get(id = user_id)
+    user=None
+    try:
+        user = User.objects.get(id = user_id)
+    except Exception:
+        return Response("User with id "+str(user_id)+" does not exist", status=status.HTTP_400_BAD_REQUEST)
     serializer = UserSerializer(user)
     return Response(serializer.data)
 
@@ -16,4 +21,5 @@ def postUser(request):
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-    return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
