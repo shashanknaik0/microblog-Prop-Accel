8dbd7120f9db66d8be660d1b2851dd317def3981
Shashank Naik <shashankkrishnanaik@gmail.com>
2023-07-04 16:21:57 +0530

    Fix. change char field to email field in user model

--- users/models.py
+++ users/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class User(models.Model):
     id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=50,unique=True,null=False)
-    email = models.CharField(max_length=50,unique=True,null=False)
+    email = models.EmailField(unique=True,null=False)
     password = models.CharField(max_length=20,null=False)
