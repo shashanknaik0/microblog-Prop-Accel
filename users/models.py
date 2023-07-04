48c5c9d35dfcc66febeb716035a59efebe2abfd0
Shashank Naik <shashankkrishnanaik@gmail.com>
2023-07-04 14:23:38 +0530

    Add. user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=50,unique=True,null=False)
+    email = models.CharField(max_length=50,unique=True,null=False)
+    password = models.CharField(max_length=20,null=False)
