5f2b6a98a170f194bb014983aed51e0de6a76252
Shashank Naik <shashankkrishnanaik@gmail.com>
2023-07-05 13:01:52 +0530

    Add. post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user_id = models.ForeignKey(to=User,on_delete=models.CASCADE)
+    content = models.CharField(max_length=255,null=False)
+    post_date = models.DateTimeField(auto_now_add=True)
