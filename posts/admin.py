7291749015d36d50a456ce8284ca6d0eea410b53
Shashank Naik <shashankkrishnanaik@gmail.com>
2023-07-05 13:04:48 +0530

    Add. post to admin site

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["id", "user_id", "post_date", "content"]
+
+admin.site.register(Post, PostAdmin)
