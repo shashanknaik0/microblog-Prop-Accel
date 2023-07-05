9c96afec9dc2ef7a5b7fe3af7ee4f5566431a3f5
Shashank Naik <shashankkrishnanaik@gmail.com>
2023-07-05 12:07:13 +0530

    Fix. post data not updating password

--- users/views.py
+++ users/views.py
@@ -19,27 +19,23 @@ def getOrUpdateUser(request, user_id):
         return Response(serializer.data)
     
     elif request.method in ['PUT','PATCH']:
-            #serialize data as specified in user model
-            serializer = UserSerializer(data=request.data)
-
-            #validate as specified in model
-            if serializer.is_valid():
-                user=None
-                try:
-                    user = User.objects.get(id = user_id)
-                except Exception:
-                    return Response("User with id "+str(user_id)+" does not exist", status=status.HTTP_400_BAD_REQUEST)
+        #we can check here if we want only logged in user want to update (by using req.user.is_authenticated)
+        user=None
+        try:
+            user = User.objects.get(id = user_id)
+        except Exception:
+            return Response("User with id "+str(user_id)+" does not exist", status=status.HTTP_400_BAD_REQUEST)
                 
-                #updating fields
-                user.username = serializer.data['username']
-                user.email = serializer.data['email']
-                user.password = serializer.data['password']
-                user.first_name = serializer.data['first_name']
-                user.last_name = serializer.data['last_name']
-                #saving to database
-                user.save()
-                return Response(serializer.data)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        #updating fields
+        user.username = request.data['username']
+        user.email = request.data['email']
+        user.password = request.data['password']
+        user.first_name = request.data['first_name']
+        user.last_name = request.data['last_name']
+        #saving to database
+        user.save()
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
         
 
 @api_view(['POST'])
@@ -49,6 +45,6 @@ def postUser(request):
     #validate as specified in model
     if serializer.is_valid():
         #saving to database
-        serializer.save()
+        serializer.create(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
