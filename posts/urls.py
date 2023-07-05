5707485a8bf1ea3b6feb534b8aecfee04b467066
Shashank Naik <shashankkrishnanaik@gmail.com>
2023-07-05 12:39:34 +0530

    Add. api endpoints for post

---
+++ posts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    #as specified in the question to use same path for Get, update, delete request
+    path('posts/<int:post_id>/', views.getUpdateOrDeletePost),
+    path('posts/', views.createPost),
+]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
 # Create your views here.
+@api_view(['GET','PUT', 'PATCH', 'DELETE'])
+def getUpdateOrDeletePost(request, post_id):
+    return Response()
+        
+
+@api_view(['POST'])
+def createPost(request):
+    return Response()
